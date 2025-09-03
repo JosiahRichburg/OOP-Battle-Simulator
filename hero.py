eb26bcccc4718280a69ba884a2f227897f910fb0
JosiahRichburg <richburgjosiah877@gmail.com>
2025-09-03 09:54:58 -0500

    Game Code Completed
    Fixed Goblin and Hero code

--- hero.py
+++ hero.py
@@ -1,3 +1,5 @@
+import random
+
 class Hero:
     """
     This is our hero blueprint.
@@ -13,16 +15,27 @@ class Hero:
     """
     
     def __init__(self, name):
-        #TODO Set the hero's name.
-        #TODO Set the hero's health. You might give the hero more health than a goblin.
-        #TODO Set the hero's attack power. Should it be more consistent than the goblin's?
+        self.name = name
+        self.health = 150
+        self.attack_power = random.randint(25, 50)
     
 
     def strike(self):
-        # TODO Implement the hero's attack logic. It could be stronger or more consistent than a goblin's.
+        critical = random.random() < 0.2
+        if critical:
+            print("Critical Hit!")
+            return random.randint(1, self.attack_power) * 2
+        miss = random.random() < 0.1
+        if miss:
+            print("Miss!")
+            return 0
+        return random.randint(1, self.attack_power)
     
     def receive_damage(self, damage):
-        # TODO Implement take_damage
-        # TODO We should prevent health from going into the NEGATIVE
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0       
+        print(f"{self.name} takes {damage} damage. Health is now {self.health}.")
     
-    #TODO define is_alive
+    def is_alive(self):
+        return self.health > 0
